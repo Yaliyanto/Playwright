94b3d351fab69140b74dea51a570386299bd81ed
Yal <yaliyanto97@gmail.com>
2025-01-04 21:14:24 +0700

    add new scenario

--- test_logout.py
+++ test_logout.py
@@ -1,6 +1,9 @@
 from playwright.sync_api import Page, expect
 from time import sleep
+import pytest
 
+
+#Login dengan username & passowrd yang tidak kosong sampai CO
 def test_login(page: Page):   
     page.goto("https://saucedemo.com")
     
@@ -22,6 +25,92 @@ def test_login(page: Page):
 
     page.close()
 
+#Login dengan username benar dan password yang kosong
+def test_login_negatif_password(page: Page):   
+    page.goto("https://saucedemo.com")
+    
+    page.locator('//input[@placeholder="Username"]').fill('standard_user')
+    page.locator('[data-test="password"]').fill('')
+    page.locator('[id="login-button"]').click()
+    
+    page.locator('[data-test="error"]').inner_text()
+
+    galat = page.locator('[data-test="error"]').inner_text()
+    
+    assert galat == "Epic sadface: Password is required"
+
+
+    # expect(page).to_have_title("Swag Labs")
+    # expect(page).to_have_url("https://www.saucedemo.com/inventory.html")
+    
+    # inventory_page_title = page.locator("//div[@class='app_logo']").text_content()
+    # assert inventory_page_title == 'Swag Labs'
+    
+    # page.locator('[id="add-to-cart-sauce-labs-backpack"]').click()
+    # page.locator('[data-test="shopping-cart-link"]').click()
+    # inventory_deskripsi_subtitle = page.locator('[data-test="cart-desc-label"]').text_content()
+    # assert inventory_deskripsi_subtitle == 'Description'
+    # sleep(5)
+
+    page.close()
+
+# Login dengan username yang kosong
+def test_login_negatif_username(page: Page):   
+    page.goto("https://saucedemo.com")
+    
+    page.locator('//input[@placeholder="Username"]').fill('')
+    page.locator('[data-test="password"]').fill('secret_sauce')
+    page.locator('[id="login-button"]').click()
+    
+    page.locator('[data-test="error"]').inner_text()
+
+    galat = page.locator('[data-test="error"]').inner_text()
+    
+    assert galat == "Epic sadface: Username is required"
+
+
+    # expect(page).to_have_title("Swag Labs")
+    # expect(page).to_have_url("https://www.saucedemo.com/inventory.html")
+    
+    # inventory_page_title = page.locator("//div[@class='app_logo']").text_content()
+    # assert inventory_page_title == 'Swag Labs'
+    
+    # page.locator('[id="add-to-cart-sauce-labs-backpack"]').click()
+    # page.locator('[data-test="shopping-cart-link"]').click()
+    # inventory_deskripsi_subtitle = page.locator('[data-test="cart-desc-label"]').text_content()
+    # assert inventory_deskripsi_subtitle == 'Description'
+    # sleep(5)
+
+    page.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # test = [('standar_user','','Epic sadface: Password is required'),
 #         ('','secret_sauce','Epic sadface: Username is required'),]
 
@@ -36,3 +125,10 @@ def test_login(page: Page):
 #     galat = page.locator('[data-test="error"]').inner_text()
     
 #     assert galat == error
+
+
+
+
+
+
+
